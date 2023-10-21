db3f21e9fb11194e0215ba4ee8db57200ecc32d0
Utkarsh Arora <arorautkarsh2002@gmail.com>
2023-10-21 15:56:02 +0530

    Sample Pushed

---
+++ ProfileImage/app/src/main/java/com/utkarsh/profileimage/MainActivity.java
@@ -0,0 +1,47 @@
+package com.utkarsh.profileimage;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btnCamera);
+        imageView = findViewById(R.id.image_Camera);
+
+        button.setOnClickListener(v -> mgetContent.launch("image/*"));
+
+
+
+
+    }
+
+    ActivityResultLauncher<String> mgetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if(result!=null){
+                        imageView.setImageURI(result);
+                    }
+                }
+            });
+}
